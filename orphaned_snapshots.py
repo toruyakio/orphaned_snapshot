027840b2776fb525b82b0ba82a5e0d13a171a623
toruyakio <toru.yakio@gmail.com>
2023-03-16 08:03:25 +0900

    Output descriptions so that we can be aware with it

--- orphaned_snapshots.py
+++ orphaned_snapshots.py
@@ -6,19 +6,37 @@ import botocore
 
 ec2 = boto3.client('ec2')
 
+
+# If you would like to work with specific Snapshot IDs which you are already aware with.
+
 snapshotids=[
    'snap-07722fa9e9974c95f',
    'snap-02e2fcee42057f27f',
    'snap-05048b5bd1464a398', 
    'snap-012177594985fb230',
-   'snap-054fd31234aaa2107'
+   'snap-054fd31234aaa2107',
+   'snap-744b7efb',
+   'snap-0405eba2e66b8f59d'
    ]
 
 awsids=[
    'self'
 ]
 
+
+# Example in case that you would like to work with AMI related Snapshot IDs.
+
+filters=[
+   {
+      'Name': 'description',
+      'Values': [
+         '*ami-*'
+      ]
+   }
+]
+
 # response = ec2.describe_snapshots(SnapshotIds=snapshotids)
+# response = ec2.describe_snapshots(Filters=filters, OwnerIds=awsids)
 response = ec2.describe_snapshots(OwnerIds=awsids)
 snapshots = response["Snapshots"]
 
@@ -28,6 +46,7 @@ for snapshot in snapshots:
    snapshot_id = snapshot["SnapshotId"]
    volume_id = snapshot["VolumeId"]
    taken_on = snapshot["StartTime"].date()
+   description = snapshot["Description"]
    ids=[volume_id]
    try :
       response = ec2.describe_volumes(VolumeIds=ids)
@@ -36,9 +55,9 @@ for snapshot in snapshots:
          vid = volume["VolumeId"]
          from_sid = volume["SnapshotId"]
          restored_date = volume["CreateTime"].date()
-      description = vid + " was recovered from " + from_sid + " on " + str(restored_date) 
+      guidance = vid + " was recovered from " + from_sid + " on " + str(restored_date) 
    except:
-      description = "Missing parent volume, please check with your admin if this is cost efficient."
+      guidance = "Missing parent volume, please check with your admin if this is cost efficient."
    finally:
-      print('{} {} is snapshot for {} taken on {}. {}'.format(count, snapshot_id, volume_id, taken_on, description))
+      print('{} {} is snapshot for {} taken on {}. {}, {}'.format(count, snapshot_id, volume_id, taken_on, guidance, description))
       count += 1
